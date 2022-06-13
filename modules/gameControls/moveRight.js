88737b2e9fec1a567a47439b4273e647909218bd
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-06-13 15:25:15 -0500

    moveRight logic

---
+++ modules/gameControls/moveRight.js
@@ -0,0 +1,18 @@
+import { tetroidTemplates as tetroid } from "../tetroidSetup/initializeTetroids.js";
+import {gridSettings as grid} from "../gameAreaSetup/gridSettings.js";
+
+// Logic for arrow and button to move right
+export const moveRight = () => {
+    let isRightSide = false;
+    let curTetroid = tetroid.array[tetroid.curTemplateId];
+    curTetroid.curPosTiles.forEach((tilePos) => {
+        // Set a flag if the shape is already on the right side of the screen
+        if ((tilePos) % grid.tilesWide == 9) {isRightSide = true;}
+    })
+    // If the shape is NOT on the right side of the screen, shift the shape one square right
+    if (!isRightSide) {
+        if (curTetroid.canMoveTetroid(1)) {
+            tetroid.array[tetroid.curTemplateId].updatePos();
+        }   
+    }
+}
