dce824e6ef96fe269f31992fb0420c91824ab586
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-06-12 20:19:04 -0500

    modalsSelector object created

---
+++ modules/modals.js
@@ -0,0 +1,10 @@
+const modalSelectors = {
+    modalSelector: document.querySelector("#pause-modal"),
+    modalContentSelector: document.querySelector(".pause-modal-content"),
+    instructionModalSelector: document.querySelector("#instruction-modal"),
+    instructionModalContent: document.querySelector(".instruction-modal-content"),
+    gameOverSelector: document.querySelector("#game-over-modal"),
+    gameOverContentSelector: document.querySelector(".game-over-modal-content")
+};
+
+export {modalSelectors};
