49c6cb8c62e03ef30e319f1bfaf61346131b567b
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-06-12 20:33:54 -0500

    gridSettings object

---
+++ modules/gridSettings.js
@@ -0,0 +1,9 @@
+export const gridSettings = {
+    tileDimension: 0,   // Tile size set dynamically
+    tilesWide: 10,      // 10 tiles per row
+    tilesHigh: 15,      // 15 rows
+    gridWidth: 0,       // Width of game grid set dynamically
+    gridHeight: 0,      // Height of the game grid set dynamically
+    gridSelector: '',   // DOM selector for the game grid
+    tileArr: []         // Holds DOM selectors for each tile
+}
