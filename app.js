3848d07d79d76b329ca8ab02f601c06403e700ec
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-06-12 19:57:26 -0500

    Instantiate and initialize ShapeStats

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import {oneLineCleared, twoLinesCleared, threeLinesCleared, fourLinesCleared, to
 
 import {currentScore, highestScore, scoreArr} from './modules/initializeScoreStats.js';
 
+import {lineShapeStats, lShapeStats, revLShapeStats, tShapeStats, sShapeStats, revSShapeStats, squareShapeStats, shapeStatArr} from './modules/initializeShapeStats.js';
+
 //Keeps track of the starting and current fall rate of the shape (1 square in # of milliseconds)
 const fallInterval = {         
     initial: 800,       
---
+++ modules/initializeShapeStats.js
@@ -0,0 +1,18 @@
+import { ShapeStats } from "./shapeStats.js";
+
+// Instantiate all ShapeStats class objects
+const lineShapeStats = new ShapeStats('line-shape', [1, 3, 5, 7]);  
+const lShapeStats = new ShapeStats('l-shape', [3, 5, 6, 7]);
+const revLShapeStats = new ShapeStats('rev-l-shape', [2, 3, 5, 7])
+const tShapeStats = new ShapeStats('t-shape', [3, 4, 5, 7]);
+const sShapeStats = new ShapeStats('s-shape', [3, 4, 5, 6]);
+const revSShapeStats = new ShapeStats('rev-s-shape', [2, 4, 5, 7]);
+const squareShapeStats = new ShapeStats('square-shape', [2, 4, 3, 5]);
+
+// Store all ShapeStat objects in shapeStatArr
+const shapeStatArr = [lineShapeStats, lShapeStats, revLShapeStats, tShapeStats, sShapeStats, revSShapeStats, squareShapeStats];
+
+// Initialize ShapeStats
+shapeStatArr.forEach(item => {item.initialize()});
+
+export {lineShapeStats, lShapeStats, revLShapeStats, tShapeStats, sShapeStats, revSShapeStats, squareShapeStats, shapeStatArr};
